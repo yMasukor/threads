41618bc2c448add8cb3e821752dcbebb3f9f97fe
Yasin Masukor <yasinmasukor@Emmas-iMac.local>
2015-02-17 01:02:34 +1100

    removed hardcoded port

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var io = require('socket.io')(http);
 var players = [];
 var viewerId;
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
@@ -95,6 +96,6 @@ setInterval(function(){
 
 
 
-http.listen(3000, function(){
+http.listen(app.get('port'), function(){
 	console.log('listening on *:3000');
 });
